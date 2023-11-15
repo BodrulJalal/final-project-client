58e53192ea571b9f6fd864f6c1229cdd0d899a56
Justin Chu <93137213+Justin5802@users.noreply.github.com>
2023-11-15 15:05:31 -0500

    Update StudentView.js

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,14 +4,36 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
 const StudentView = (props) => {
-  const { student } = props;
+  const { student, deleteStudent } = props;
 
   // Render a single Student view 
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+      <div style={{display: 'inline-flex', alignItems: 'center'}}>
+      <img src={student.imageUrl} alt="Student Pic" style={{ width: 'auto', height: '64px', paddingRight: '16px' }}/>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      </div>
+
+      {student.campus ? (
+        <Link to={`/campus/${student.campus.id}`}>
+          <h3>{student.campus.name}</h3>
+        </Link>
+      ) : (
+        <p>No campus</p>
+      )}
+
+      
+      <p>Email: {student.email}</p>
+      <p>GPA: {student.gpa}</p>
+
+      <div style={{ display: 'flex', gap: '10px' }}> 
+        <Link to={`/editstudent/${student.id}`}><button>Edit</button></Link>
+        <Link to={`/students`}>
+          <button className="delete" onClick={() => deleteStudent(student.id)}>Delete</button>
+        </Link>
+      </div>
     </div>
   );
 
