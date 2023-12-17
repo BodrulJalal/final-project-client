45a09637eac907f1280c9e3e782fb424547efc06
BodrulJalal <bodrul.jalal31@myhunter.cuny.edu>
2023-12-16 21:02:44 -0500

    added student views

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -7,12 +7,16 @@ It constructs a React component to display the single student view page.
 import { Link } from "react-router-dom";
 const StudentView = (props) => {
   const { student, deleteStudent } = props;
+  const imageUrl = student.imageUrl || "https://www.esm.rochester.edu/uploads/employee-or-student-icon.jpg";
 
   // Render a single Student view 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+      <div><p></p></div>
       <div style={{display: 'inline-flex', alignItems: 'center'}}>
-      <img src={student.imageUrl} alt="Student Pic" style={{ width: 'auto', height: '64px', paddingRight: '16px' }}/>
+      <img src={imageUrl} alt="Warning: Student Pic Couldn't Be Loaded" style={{ width: 'auto', height: '128px', paddingRight: '16px' }}/>
+      </div>
+      <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
       </div>
 
@@ -21,12 +25,14 @@ const StudentView = (props) => {
           <h3>{student.campus.name}</h3>
         </Link>
       ) : (
-        <p>No campus</p>
+        <p>No Campus Associated With This Student</p>
       )}
 
       
       <p>Email: {student.email}</p>
-      <p>GPA: {student.gpa}</p>
+      {student.gpa && <p>GPA: {student.gpa.toFixed(1)}</p>}
+      {/* <p>GPA: {student.gpa}</p> */}
+      <p>Student ID: {student.id}</p>
 
       <div style={{ display: 'flex', gap: '10px' }}> 
         <Link to={`/editstudent/${student.id}`}><button>Edit</button></Link>
